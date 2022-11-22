96684119edc1eccaa1ac6b6064a2d5397209f255
GreenyBeany <matusondrejka3@gmail.com>
2022-11-22 12:58:46 +0100

    UPdate 2.05

--- Python_S/08Obsah_Trojuholnika.py
+++ Python_S/08Obsah_Trojuholnika.py
@@ -1,5 +1,10 @@
 # Napíšte program na výpočet obsahu trojuholníka so základňou z, výškou v.
 # Doplňte ho prehľadným spôsobom o testovanie, či je obsah zo zadaného intervalu (D,H).
+def test():
+    if a<S<b:
+        print('Zadaný obsah ',S,' je v intervale <',a,',',b,'>')
+    else:
+        print('Zadaný obsah ',S,' nie je v intervale <',a,',',b,'>')
 
 print('Obsah Trojuholníka')
 print('---------------------------------------------------------------')
@@ -11,8 +16,5 @@ print('Obsah trojuholníka je:',str(S),"cm\u00b2")
 
 a = float(input('Je zadaný obsah v intervale od:'))
 b = float(input('                            do:'))
-if a<S<b:
-    print('Zadaný obsah ',S,' je v intervale <',a,',',b,'>')
-else:
-    print('Zadaný obsah ',S,' nie je v intervale <',a,',',b,'>')
+test()
 
