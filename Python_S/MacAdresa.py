3ab2dbad5b508f7548440d9a8881eaa310ef927f
GreenyBeany <matusondrejka3@gmail.com>
2022-12-30 12:20:20 +0100

    Python

---
+++ Python_S/MacAdresa.py
@@ -0,0 +1,13 @@
+# Program na zistenie MAC adresy počítača a následného zisťovania počtu znakov
+from getmac import get_mac_address as gma #najskôr pridáme balík, ktorý nám uľahčí postup (pri prvom použití treba cez terminál zadať pip install get-mac)
+adresa = gma()
+print('MAC adresa tohoto počítača je:',adresa.upper()) #upper je tam preto aby mac adresa bola napísaná veľkým písmom
+
+znak = str(input('Zadajte znak, ktorý chcete nájsť v MAC adrese: ')) # Získame znak, ktorý chceme nájsť
+
+
+if znak in adresa:
+    pocet = adresa.count(znak)
+    print('V adrese MAC sa znak nachádza',str(pocet)+'x') # Ak sa znak nachádza v adrese dostaneme odpoveď s množstovm 
+else:
+    print('Znak sa nenachádza v MAC adrese') # Ak sa znak nenachádza v adrese dostaneme na to odpoveď
