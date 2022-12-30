5a47a775a5317f9d8012cf879394dd25450a88cc
GreenyBeany <matusondrejka3@gmail.com>
2022-12-30 18:34:27 +0100

    Python

--- Python_S/MacAdresa.py
+++ Python_S/MacAdresa.py
@@ -7,7 +7,7 @@ znak = str(input('Zadajte znak, ktorý chcete nájsť v MAC adrese: ')) # Získa
 
 
 if znak in adresa:
-    pocet = adresa.count(znak)
+    pocet = adresa.count(znak) # použitím count() si môžeme uľahčiť postup a je to v podstate jediné čo nám stačí urobiť
     print('V adrese MAC sa znak nachádza',str(pocet)+'x') # Ak sa znak nachádza v adrese dostaneme odpoveď s množstovm 
 else:
     print('Znak sa nenachádza v MAC adrese') # Ak sa znak nenachádza v adrese dostaneme na to odpoveď
